0b926b43a9d91ce9182d6b07e467a647894fc062
iankuria668 <iankuria668@gmail.com>
2024-04-28 11:43:24 +0300

    links created

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import user from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio && <p>{user.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <p>{user.bio}</p>
+      <ul>
+        <li>
+          <a href="https://github.com/liza">GitHub</a>
+          </li>
+        <li>
+          <a href="https://www.linkedin.com/in/liza/">LinkedIn</a>
+          </li>
+      </ul>
     </div>
   );
 }
---
+++ src/components/links.js
@@ -0,0 +1,8 @@
+function Links(){
+    return(
+        <div>
+            <a href="https://github.com/liza">github</a>
+            <a href="https://www.linkedin.com/in/liza/">linkedin</a>
+        </div>
+    )
+}
